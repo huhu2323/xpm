8c030c74618c1cbd7998a4e43c4c6ce415746b33
Liam Demafelix <liamdemafelix.n@gmail.com>
2017-12-19 13:57:33 +0800

    1.0

---
+++ XPM/Form1.Designer.vb
@@ -0,0 +1,198 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.grpNewProject = New System.Windows.Forms.GroupBox()
+        Me.btnCreate = New System.Windows.Forms.Button()
+        Me.chkRunLaravelNew = New System.Windows.Forms.CheckBox()
+        Me.chkLaravel = New System.Windows.Forms.CheckBox()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.txtProjectName = New System.Windows.Forms.TextBox()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.ssGeneric = New System.Windows.Forms.StatusStrip()
+        Me.tslStatus = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.bwServiceRestart = New System.ComponentModel.BackgroundWorker()
+        Me.bwRunLaravel = New System.ComponentModel.BackgroundWorker()
+        Me.refreshList = New System.Windows.Forms.Timer(Me.components)
+        Me.lvProjects = New System.Windows.Forms.ListBox()
+        Me.lblCredit = New System.Windows.Forms.LinkLabel()
+        Me.grpNewProject.SuspendLayout()
+        Me.ssGeneric.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'grpNewProject
+        '
+        Me.grpNewProject.Controls.Add(Me.btnCreate)
+        Me.grpNewProject.Controls.Add(Me.chkRunLaravelNew)
+        Me.grpNewProject.Controls.Add(Me.chkLaravel)
+        Me.grpNewProject.Controls.Add(Me.lblName)
+        Me.grpNewProject.Controls.Add(Me.txtProjectName)
+        Me.grpNewProject.Location = New System.Drawing.Point(12, 12)
+        Me.grpNewProject.Name = "grpNewProject"
+        Me.grpNewProject.Size = New System.Drawing.Size(335, 155)
+        Me.grpNewProject.TabIndex = 0
+        Me.grpNewProject.TabStop = False
+        Me.grpNewProject.Text = "New Project"
+        '
+        'btnCreate
+        '
+        Me.btnCreate.Location = New System.Drawing.Point(130, 114)
+        Me.btnCreate.Name = "btnCreate"
+        Me.btnCreate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCreate.TabIndex = 4
+        Me.btnCreate.Text = "&Create"
+        Me.btnCreate.UseVisualStyleBackColor = True
+        '
+        'chkRunLaravelNew
+        '
+        Me.chkRunLaravelNew.AutoSize = True
+        Me.chkRunLaravelNew.Enabled = False
+        Me.chkRunLaravelNew.Location = New System.Drawing.Point(101, 85)
+        Me.chkRunLaravelNew.Name = "chkRunLaravelNew"
+        Me.chkRunLaravelNew.Size = New System.Drawing.Size(115, 19)
+        Me.chkRunLaravelNew.TabIndex = 3
+        Me.chkRunLaravelNew.Text = "&Run 'laravel new'"
+        Me.chkRunLaravelNew.UseVisualStyleBackColor = True
+        '
+        'chkLaravel
+        '
+        Me.chkLaravel.AutoSize = True
+        Me.chkLaravel.Location = New System.Drawing.Point(101, 60)
+        Me.chkLaravel.Name = "chkLaravel"
+        Me.chkLaravel.Size = New System.Drawing.Size(137, 19)
+        Me.chkLaravel.TabIndex = 2
+        Me.chkLaravel.Text = "Use &Laravel Template"
+        Me.chkLaravel.UseVisualStyleBackColor = True
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(17, 34)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(39, 15)
+        Me.lblName.TabIndex = 1
+        Me.lblName.Text = "Name"
+        '
+        'txtProjectName
+        '
+        Me.txtProjectName.Location = New System.Drawing.Point(101, 31)
+        Me.txtProjectName.Name = "txtProjectName"
+        Me.txtProjectName.Size = New System.Drawing.Size(213, 23)
+        Me.txtProjectName.TabIndex = 0
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(272, 368)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(75, 23)
+        Me.btnDelete.TabIndex = 2
+        Me.btnDelete.Text = "&Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'ssGeneric
+        '
+        Me.ssGeneric.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tslStatus})
+        Me.ssGeneric.Location = New System.Drawing.Point(0, 402)
+        Me.ssGeneric.Name = "ssGeneric"
+        Me.ssGeneric.Size = New System.Drawing.Size(359, 22)
+        Me.ssGeneric.TabIndex = 3
+        Me.ssGeneric.Text = "StatusStrip1"
+        '
+        'tslStatus
+        '
+        Me.tslStatus.Name = "tslStatus"
+        Me.tslStatus.Size = New System.Drawing.Size(39, 17)
+        Me.tslStatus.Text = "Ready"
+        '
+        'bwServiceRestart
+        '
+        '
+        'bwRunLaravel
+        '
+        '
+        'refreshList
+        '
+        Me.refreshList.Interval = 30000
+        '
+        'lvProjects
+        '
+        Me.lvProjects.FormattingEnabled = True
+        Me.lvProjects.ItemHeight = 15
+        Me.lvProjects.Location = New System.Drawing.Point(12, 173)
+        Me.lvProjects.Name = "lvProjects"
+        Me.lvProjects.Size = New System.Drawing.Size(335, 184)
+        Me.lvProjects.TabIndex = 4
+        '
+        'lblCredit
+        '
+        Me.lblCredit.AutoSize = True
+        Me.lblCredit.Location = New System.Drawing.Point(9, 372)
+        Me.lblCredit.Name = "lblCredit"
+        Me.lblCredit.Size = New System.Drawing.Size(188, 15)
+        Me.lblCredit.TabIndex = 6
+        Me.lblCredit.TabStop = True
+        Me.lblCredit.Text = "https://github.com/liamdemafelix"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(359, 424)
+        Me.Controls.Add(Me.lblCredit)
+        Me.Controls.Add(Me.lvProjects)
+        Me.Controls.Add(Me.ssGeneric)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.grpNewProject)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "XPM"
+        Me.grpNewProject.ResumeLayout(False)
+        Me.grpNewProject.PerformLayout()
+        Me.ssGeneric.ResumeLayout(False)
+        Me.ssGeneric.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents grpNewProject As GroupBox
+    Friend WithEvents btnCreate As Button
+    Friend WithEvents chkRunLaravelNew As CheckBox
+    Friend WithEvents chkLaravel As CheckBox
+    Friend WithEvents lblName As Label
+    Friend WithEvents txtProjectName As TextBox
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents ssGeneric As StatusStrip
+    Friend WithEvents tslStatus As ToolStripStatusLabel
+    Friend WithEvents bwServiceRestart As System.ComponentModel.BackgroundWorker
+    Friend WithEvents bwRunLaravel As System.ComponentModel.BackgroundWorker
+    Friend WithEvents refreshList As Timer
+    Friend WithEvents lvProjects As ListBox
+    Friend WithEvents lblCredit As LinkLabel
+End Class
---
+++ XPM/Form1.vb
@@ -0,0 +1,228 @@
+﻿Imports System.ServiceProcess
+Imports System.Threading
+
+Public Class frmMain
+
+    ' Application Variables
+    Dim XAMPP_ROOT As String = "C:\xampp\"
+    Dim XPM_ROOT As String = Application.StartupPath & "\"
+
+    ' Service Declarations
+    Dim ApacheService As String = "Apache2.4"
+    Dim MySQLService As String = "mysql"
+
+    ' Re-usable variables
+    Dim restartSrv As String = ""
+    Dim projectName As String = ""
+    Dim projectConfFile As String = ""
+
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Determine XAMPP Root
+        If My.Computer.FileSystem.FileExists(XPM_ROOT & "root.txt") Then
+            XAMPP_ROOT = My.Computer.FileSystem.ReadAllText(XPM_ROOT & "root.txt")
+        End If
+
+        If XAMPP_ROOT.EndsWith("\") = False Then
+            XAMPP_ROOT = XAMPP_ROOT & "\"
+        End If
+
+        ' Let's see if XAMPP is installed.
+        If My.Computer.FileSystem.DirectoryExists(XAMPP_ROOT) = False Then
+            MessageBox.Show("XAMPP is not installed on this system. If you have a different location for XAMPP, create a root.txt file in the XMP folder that contains the absolute path to your xampp folder." & vbNewLine & vbNewLine & "We are currently looking at: " & XAMPP_ROOT, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End
+        End If
+
+        ' Generate the vhosts folder
+        If My.Computer.FileSystem.DirectoryExists(XAMPP_ROOT & "apache\conf\vhosts") = False Then
+            ' Create
+            My.Computer.FileSystem.CreateDirectory(XAMPP_ROOT & "apache\conf\vhosts")
+            ' Rewrite the httpd-vhosts.conf file
+            Dim vhdata As String = $"
+NameVirtualHost *:80
+<VirtualHost *:80>
+	ServerAdmin root@localhost
+	DocumentRoot ""{XAMPP_ROOT}htdocs""
+	ServerName localhost
+	ErrorLog ""logs/localhost-error.log""
+	CustomLog ""logs/localhost-access.log"" common
+</VirtualHost>
+
+IncludeOptional conf/vhosts/*.conf"
+            My.Computer.FileSystem.WriteAllText(XAMPP_ROOT & "apache\conf\extra\httpd-vhosts.conf", vhdata, False)
+        End If
+
+        ' Start Timer
+        refreshList.Start()
+
+        ' Initialize
+        For Each file As String In IO.Directory.GetFiles(XAMPP_ROOT & "apache\conf\vhosts", "*.*")
+            lvProjects.Items.Add(file)
+        Next
+    End Sub
+
+    Private Sub bwServiceRestart_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles bwServiceRestart.DoWork
+        For Each s As ServiceController In ServiceController.GetServices()
+            If s.ServiceName = restartSrv Then
+                tslStatus.Text = "Restarting service " & restartSrv & "..."
+                If s.Status = ServiceControllerStatus.Running Then
+                    s.Stop()
+                    s.WaitForStatus(ServiceControllerStatus.Stopped)
+                    s.Start()
+                    s.WaitForStatus(ServiceControllerStatus.Running)
+                ElseIf s.Status = ServiceControllerStatus.Stopped Then
+                    s.Start()
+                    s.WaitForStatus(ServiceControllerStatus.Running)
+                End If
+            End If
+        Next
+    End Sub
+
+    Private Sub btnCreate_Click(sender As Object, e As EventArgs) Handles btnCreate.Click
+        ' Disable All Controls
+        btnCreate.Enabled = False
+        btnDelete.Enabled = False
+        txtProjectName.ReadOnly = True
+        chkLaravel.Enabled = False
+        chkRunLaravelNew.Enabled = False
+
+        ' Validate
+        projectName = txtProjectName.Text()
+        If System.Text.RegularExpressions.Regex.IsMatch(projectName, "^[a-z0-9]+$") = False Then
+            MessageBox.Show("Invalid project name. XMP only accepts alphanumeric characters.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            btnCreate.Enabled = True
+            btnDelete.Enabled = True
+            txtProjectName.ReadOnly = False
+            chkLaravel.Enabled = True
+            If chkLaravel.Checked Then
+                chkRunLaravelNew.Enabled = True
+            End If
+            Exit Sub
+        End If
+
+        projectConfFile = XAMPP_ROOT & "apache\conf\vhosts\" & projectName & ".local.conf"
+        If My.Computer.FileSystem.FileExists(projectConfFile) Then
+            btnCreate.Enabled = True
+            btnDelete.Enabled = True
+            txtProjectName.ReadOnly = False
+            chkLaravel.Enabled = True
+            If chkLaravel.Checked Then
+                chkRunLaravelNew.Enabled = True
+            End If
+            MessageBox.Show("A project of the same name already exists.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            Exit Sub
+        End If
+
+        ' Write
+        tslStatus.Text = "Writing configuration..."
+        Dim drAppend = ""
+        If chkLaravel.Checked Then
+            drAppend = "\public"
+        End If
+        Dim vh As String = $"
+<VirtualHost *:80>
+	ServerAdmin root@localhost
+	DocumentRoot ""{XAMPP_ROOT}htdocs\{projectName}{drAppend}""
+	ServerName {projectName}.local
+	ErrorLog ""logs/localhost-error.log""
+	CustomLog ""logs/localhost-access.log"" common
+</VirtualHost>"
+        My.Computer.FileSystem.WriteAllText(projectConfFile, vh, False)
+
+        ' Do we run laravel new?
+        If chkRunLaravelNew.Checked = True Then
+            If chkLaravel.Checked = True Then
+                tslStatus.Text = "Creating Laravel application..."
+                bwRunLaravel.RunWorkerAsync()
+                While bwRunLaravel.IsBusy
+                    Thread.Sleep(200)
+                    Application.DoEvents()
+                End While
+            End If
+        End If
+
+        ' Restart Apache
+        restartSrv = "Apache2.4"
+        bwServiceRestart.RunWorkerAsync()
+
+
+        lvProjects.Items.Clear()
+        Dim AllowedExtension As String = "conf"
+        For Each file As String In IO.Directory.GetFiles(XAMPP_ROOT & "apache\conf\vhosts", "*.*")
+            lvProjects.Items.Add(file)
+        Next
+
+        ' Done
+        MessageBox.Show("Project created.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        btnCreate.Enabled = True
+        btnDelete.Enabled = True
+        txtProjectName.ReadOnly = False
+        chkLaravel.Enabled = True
+        If chkLaravel.Checked Then
+            chkRunLaravelNew.Enabled = True
+        End If
+    End Sub
+
+    Private Sub bwServiceRestart_RunWorkerCompleted(sender As Object, e As System.ComponentModel.RunWorkerCompletedEventArgs) Handles bwServiceRestart.RunWorkerCompleted
+        tslStatus.Text = "Ready"
+    End Sub
+
+    Private Sub bwRunLaravel_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles bwRunLaravel.DoWork
+        Process.Start("cmd.exe", "/c cd " & XAMPP_ROOT & "htdocs\ && laravel new " & projectName)
+    End Sub
+
+    Private Sub refreshList_Tick(sender As Object, e As EventArgs) Handles refreshList.Tick
+        lvProjects.Items.Clear()
+        Dim AllowedExtension As String = "conf"
+        For Each file As String In IO.Directory.GetFiles(XAMPP_ROOT & "apache\conf\vhosts", "*.*")
+            lvProjects.Items.Add(file)
+        Next
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        If lvProjects.SelectedItem = "" Then
+            MessageBox.Show("Select a project to delete first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End If
+
+        Dim result As Integer = MessageBox.Show("Do you also want to delete the project folder?", "Delete Project", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question)
+        If result = DialogResult.Cancel Then
+            ' Exit Sub
+            Exit Sub
+        ElseIf result = DialogResult.No Then
+            My.Computer.FileSystem.DeleteFile(lvProjects.SelectedItem)
+        ElseIf result = DialogResult.Yes Then
+            My.Computer.FileSystem.DeleteFile(lvProjects.SelectedItem)
+            Dim projdir As String = XAMPP_ROOT & "htdocs\" & lvProjects.SelectedItem().ToString().Replace(XAMPP_ROOT & "apache\conf\vhosts", "").ToString().Replace(".local.conf", "")
+            If My.Computer.FileSystem.DirectoryExists(projdir) Then
+                My.Computer.FileSystem.DeleteDirectory(projdir, FileIO.DeleteDirectoryOption.DeleteAllContents)
+            End If
+        End If
+
+        ' Restart Apache
+        restartSrv = "Apache2.4"
+        bwServiceRestart.RunWorkerAsync()
+
+
+        lvProjects.Items.Clear()
+        Dim AllowedExtension As String = "conf"
+        For Each file As String In IO.Directory.GetFiles(XAMPP_ROOT & "apache\conf\vhosts", "*.*")
+            lvProjects.Items.Add(file)
+        Next
+
+        MessageBox.Show("Project deleted.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub chkLaravel_CheckedChanged(sender As Object, e As EventArgs) Handles chkLaravel.CheckedChanged
+        If chkLaravel.Checked Then
+            chkRunLaravelNew.Enabled = True
+        Else
+            chkRunLaravelNew.Checked = False
+            chkRunLaravelNew.Enabled = False
+        End If
+    End Sub
+
+    Private Sub lblCredit_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles lblCredit.LinkClicked
+        Process.Start("https://github.com/liamdemafelix")
+    End Sub
+End Class
